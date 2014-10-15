3dd6d3747e4499892a2c22723effeaed46979283
Sebastian Müller <senpo@posteo.de>
2014-10-15 17:10:52 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+TutCheck
+========
+
+## Summary
+
+TutCheck is a small script that checks if a website changes during runtime. This can be useful when a registration or a result is unlocked online without a specific time. When the website changes, an alert sound (.wav file) plays (tested under Mac OS X).
+
+## Usage
+
+Simply run `python tutcheck.py` in terminal.
---
+++ tutcheck.py
@@ -0,0 +1,87 @@
+# TutCheck alerts when a website changes
+
+# Copyright (C) 2014  Sebastian Müller
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import urllib
+from AppKit import NSSound
+from time import sleep
+
+def alert():
+	class sound:
+	    def __init__(self, file):
+	        self._sound = NSSound.alloc()
+	        self._sound.initWithContentsOfFile_byReference_(file, True)
+	    def play(self): self._sound.play()
+	    def stop(self): self._sound.stop()
+	    def is_playing(self): return self._sound.isPlaying()
+
+	for i in range(100):
+	        s = sound("hurricane_ton3.wav")
+	        s.play()
+	        sleep(3)
+
+def zeit(sec):
+	if float(sec) < 10:
+		sec1 = "0"+str(sec)
+	else:
+		sec1 = str(sec)
+
+	return sec1
+
+from time import localtime, sleep
+
+print "*** TutCheck by Sebastian Müller ***"
+useold = raw_input("Use last website for comparison? (Y/N) ")
+if useold == "Y":
+	d = open("orig.html", "r")
+	orig = d.read()
+	d.close()
+	d = open("url.txt", "r")
+	url = d.read()
+	d.close()
+else:
+	url = raw_input("Target URL: ")
+	print "URL:", url
+	f = urllib.urlopen(url)
+	orig = f.read()
+	f.close()
+	d = open("orig.html", "w")
+	d.write(orig)
+	d.close
+  	d = open("url.txt", "w")
+	d.write(url)
+	d.close
+
+
+print "Original website has been cached for comparison."
+
+while True:
+	time = localtime()
+	sec = zeit(time[5])
+	hour = zeit(time[3])
+	minute = zeit(time[4])
+	loct = hour+":"+minute+":"+sec
+
+	g = urllib.urlopen(url)
+	comp = g.read()
+	g.close()
+
+	if comp != orig:
+		print loct, "!ALERT! Website has changed !ALERT!"
+		alert()
+	else:
+		print loct, "No changes yet"
+
